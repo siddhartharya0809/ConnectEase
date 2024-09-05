5105a8d33f0202998af7856d713fd78505a58381
Siddharth Arya <63834479+siddhartharya0809@users.noreply.github.com>
2024-09-05 10:08:46 +0530

    tailwind and typescript configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,33 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        landingText: ["broadway"],
+        customHeading: ["headingFont"],
+        customSubHeading: ["subHeadingFont"],
+      },
+      fontSize: {
+        sm: "0.8rem",
+        base: "1rem",
+        xl: "1.25rem",
+        "2xl": "2rem",
+        "3xl": "3rem",
+        "4xl": "4rem",
+        "5xl": "5rem",
+        "6xl": "6rem",
+        "7xl": "7rem",
+        "8xl": "8rem",
+        "9xl": "11.451em",
+      },
+      screens: {
+        sm: "640px", // => @media (min-width: 640px) { ... }
+        md: "768px", // => @media (min-width: 376px) { ... }
+        lg: "1024px", // => @media (min-width: 1024px) { ... }
+        xl: "1280px", // => @media (min-width: 1280px) { ... }
+      },
+      backgroundColor: {
+        landingBg: "#70a9a1",
+      },
     },
   },
   plugins: [],
