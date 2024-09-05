7953e6363929c06a65301565adfcf3403b23dbda
Siddharth Arya <63834479+siddhartharya0809@users.noreply.github.com>
2024-09-05 10:08:32 +0530

    basic routing for home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
-
+import Landing from "@/pages/index";
+import LandingSection from "@/components/LandingSection";
 export default function Home() {
-  return <>Hello Worlds</>;
+  return (
+    <>
+      <LandingSection />
+    </>
+  );
 }
